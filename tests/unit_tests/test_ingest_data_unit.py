531e0867d6b3eb1a5dd71887e708a7a9c7cb1745
abbeymaj <abbey.majumdar@gmail.com>
2026-09-05 16:45:21 -0400

    updated unit tests

--- tests/unit_tests/test_ingest_data_unit.py
+++ tests/unit_tests/test_ingest_data_unit.py
@@ -1,4 +1,5 @@
 # Importing packages
+import os
 import pytest
 import pandas as pd
 from src.components.config_entity import DataIngestionConfig
@@ -37,3 +38,17 @@ def test_train_and_test_is_dataframe():
     _, _, train_df, test_df = ingestion.initiate_data_ingestion(create_artifacts_folder=False, save_objects=False, return_datasets=True)
     assert isinstance(train_df, pd.DataFrame)
     assert isinstance(test_df, pd.DataFrame)
+
+# Verifying that the artifacts folder creation is attempted, without touching the filesystem
+@pytest.mark.unit
+def test_create_artifacts_folder_calls_makedirs(mocker, read_raw_data):
+    mock_makedirs = mocker.patch('src.components.ingest_data.os.makedirs')
+    mocker.patch('src.components.ingest_data.pd.read_parquet', return_value=read_raw_data)
+    ingestion = IngestData()
+
+    ingestion.initiate_data_ingestion(create_artifacts_folder=True, save_objects=False, return_datasets=False)
+
+    mock_makedirs.assert_called_once_with(
+        os.path.dirname(ingestion.data_ingestion_config.train_data_path),
+        exist_ok=True
+    )
